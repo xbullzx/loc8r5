1d838a428bb3fc92a12e37bc417e0eac37007e7f
Chris VanHorn <chris_vnhrn@yahoo.com>
2021-01-10 13:56:03 -0600

    ch5 finished

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,40 +1,69 @@
-// 5.4.2 adding subdocuments
-
 const mongoose = require('mongoose');
-const dbURI = 'mongodb://localhost/Loc8r5';
-mongoose.connect(dbURI, {useNewUrlParser: true});
+const readLine = require('readline');
+
+let dbURL = 'mongodb://127.0.0.1/loc8r5';
+if (process.env.NODE_ENV === 'production') {
+  dbURL = process.env.DB_HOST || process.env.
+MONGODB_URI;
+}
+
+console.log('NODE_ENV is set to ', process.env.NODE_ENV)
+;
+
+const connect = () => {
+  setTimeout(() => mongoose.connect(dbURL, { 
+useNewUrlParser: true, useCreateIndex: true, 
+useUnifiedTopology: true }), 1000);
+}
+
 mongoose.connection.on('connected', () => {
-  console.log(`Mongoose connected to ${dbURI}`);
+  console.log('Mongoose is connected');
 });
+
 mongoose.connection.on('error', err => {
-  console.log(`Mongoose connection error: ${err}`);
+  console.log('Mongoose connection error: ' + err);
+  return connect();
 });
+
 mongoose.connection.on('disconnected', () => {
-  console.log('Mongoose disconnected');
+  console.log('Mongoose is disconnected');
 });
+
+if (process.platform === 'win32') {
+  const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.on ('SIGINT', () => {
+    process.emit("SIGINT");
+  });
+}
+
 const gracefulShutdown = (msg, callback) => {
   mongoose.connection.close( () => {
     console.log(`Mongoose disconnected through ${msg}`);
     callback();
   });
 };
-// For nodemon restarts
+
 process.once('SIGUSR2', () => {
   gracefulShutdown('nodemon restart', () => {
     process.kill(process.pid, 'SIGUSR2');
   });
 });
-// For app termination
+
 process.on('SIGINT', () => {
   gracefulShutdown('app termination', () => {
     process.exit(0);
   });
 });
-// For Heroku app termination
+
 process.on('SIGTERM', () => {
   gracefulShutdown('Heroku app shutdown', () => {
     process.exit(0);
   });
 });
+
+connect();
 
 require('./locations');
